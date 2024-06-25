30498aeb1aed252a7ec691488c0cd6d375caacd1
Alfredo Lourenço <96731660+techlou@users.noreply.github.com>
2024-06-25 17:57:15 -0300

    removed deleteByEmail endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,7 @@ const userRouter = require('express').Router();
 
 userRouter.get('/', user.get);
 userRouter.post('/', user.post);
-// userRouter.delete('/', user.deleteByID);
-userRouter.delete('/', user.deleteEmail);
+userRouter.delete('/', user.deleteByID);
 userRouter.put('/', user.edit);
 
 module.exports = userRouter;
